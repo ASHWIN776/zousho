db7a84e0b5875960bb1d9307aafb85e94149b6f9
Ashwin Anil <ashwinrockon123@gmail.com>
2024-12-17 01:47:09 +0900

    feat: Added pg function for adding page
    Transaction for:
    - Adds into pages table
    - Adds entries into page_sections

---
+++ supabase/migrations/20241216153533_add_page_function.sql
@@ -0,0 +1,39 @@
+alter table pages alter column path drop not null;
+
+create type page_section_data as (
+  content text,
+  embedding vector(1024)
+);
+
+create or replace function add_page (
+  name_input text,
+  page_section_data_input page_section_data[],
+  type_input page_type,
+  path_input text
+) 
+returns void as 
+$$
+declare
+  section_data page_section_data;
+  page_id bigint;
+begin
+
+  -- Assert that path is provided for website pages
+  IF type_input = 'website' AND path_input IS NULL THEN
+    RAISE EXCEPTION 'Path is required for website pages';
+  END IF;
+
+  -- Insert page
+  insert into pages (name, path)
+  values (name_input, path_input)
+  returning id into page_id;
+
+  -- Insert page sections 
+  foreach section_data in array page_section_data_input
+  loop
+    insert into page_sections (page_id, content, embedding)
+    values (page_id, section_data.content, section_data.embedding);
+  end loop;
+  
+end;
+$$ language plpgsql;
