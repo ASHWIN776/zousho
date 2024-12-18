48358718f90d47323ace03b2d405a5687d027333
Ashwin Anil <ashwinrockon123@gmail.com>
2024-12-19 00:30:58 +0900

    style: ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import SearchContainer from "@/components/SearchContainer";
 
 export default function Index() {
   return (
-    <main className="flex flex-col gap-y-10 justify-center items-center h-full">
+    <main className="flex flex-col gap-y-6 justify-center items-center h-full">
       <span className="text-3xl">Recall It Back!</span>
-      <div className="flex gap-x-10 border border-slate-400 rounded-md p-4 h-[500px]">
+      <div className="flex gap-x-10 border border-slate-400 rounded-md p-4 h-[500px] w-[1000px]">
         <AddUrlContainer />
         <div className="w-[2px] bg-white"></div>
         <SearchContainer />
