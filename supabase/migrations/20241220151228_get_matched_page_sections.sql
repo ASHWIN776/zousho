63106586a351f536e0f99f8787dc933fd00f3ecf
Ashwin Anil <ashwinrockon123@gmail.com>
2024-12-28 12:50:57 +0900

    db: Added user defined function get_matched_page_sections

---
+++ supabase/migrations/20241220151228_get_matched_page_sections.sql
@@ -0,0 +1,19 @@
+create or replace function get_matched_page_sections(
+  query_embedding vector(1024),
+  match_limit int,
+  type_input page_type DEFAULT NULL
+)
+returns table (page_id bigint, section_id bigint, section_content text, similarity float)
+language plpgsql
+as $$
+begin
+  return query select pages.id, page_sections.id, page_sections.content, 1 - (embedding <=> query_embedding) as similarity
+  from pages 
+  JOIN page_sections
+  ON pages.id = page_sections.page_id
+  WHERE 1 = 1
+    AND (type_input IS NULL OR pages.type = type_input)
+  ORDER BY similarity DESC
+  limit match_limit;
+end
+$$
