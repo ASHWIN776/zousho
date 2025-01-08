8530b8d30fd9b09c78fd5af9d9b7ab256d8f526f
Ashwin Anil <ashwinrockon123@gmail.com>
2025-01-08 11:51:21 +0900

    feat: Easy ui for root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import AddUrlContainer from "@/components/AddUrlContainer";
 import SearchContainer from "@/components/SearchContainer";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Index() {
   return (
-    <main className="flex flex-col justify-center items-center h-full">
-      <div className="flex gap-x-10 border border-slate-400 rounded-md p-4 h-[500px] w-[1000px]">
-        <AddUrlContainer />
-        <div className="w-[2px] bg-white"></div>
-        <SearchContainer />
+    <main className="flex flex-col justify-center items-center h-full gap-y-4">
+      <div className="flex flex-col justify-center items-center gap-y-1">
+        <span className="text-3xl">Recall</span>
+        <div className="text-zinc-400 text-md flex flex-col items-center">
+          <span>Document your learnings easily</span>
+          <span>Recall it whenever you need!</span>
+        </div>
       </div>
-      <Link href="/chat">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Chat with all your knowledge! 🤖
-        </button>
+      <Link href="/dashboard">
+        <Button>Try the App</Button>
       </Link>
     </main>
   );
