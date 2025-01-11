016a34fc54050026490543dc11d715c68d43eced
Ashwin Anil <ashwinrockon123@gmail.com>
2025-01-11 21:34:20 +0900

    feature: User prompt will be cleaned before sending it to the RAG pipeline

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,17 +1,26 @@
 import { searchPageSections } from '@/lib/actions';
+import { cleanUserPrompt } from '@/lib/helper';
 import { createUserPrompt, systemPrompt } from '@/lib/prompt';
 import { groq } from '@ai-sdk/groq';
-import { CoreMessage, streamText } from 'ai';
+import { CoreMessage, createDataStreamResponse, streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-  console.log("Messages: ", messages)
   const lastMessage = messages.at(-1).content
+  const cleanPrompt = await cleanUserPrompt(lastMessage);
 
-  console.log("Prompt of the user: ", lastMessage)
+  // If the question is not right, return an error message to the user
+  if(cleanPrompt === "0") {
+    return createDataStreamResponse({
+      execute(dataStream) {
+        dataStream.writeData("Sorry, I couldn't understand that. Please try again.")
+      },
+    })
+  }
+
   const contextList = await searchPageSections(lastMessage, "all");
   const systemMessage: CoreMessage = {
     role: "system",
--- lib/prompt.ts
+++ lib/prompt.ts
@@ -25,6 +25,17 @@ Include reasoning in <justification> tags explaining:
 - Any limitations in the available context
 `;
 
+export const cleanQuestionPrompt = `
+You are an expert in formatting questions for an AI assistant that helps users access and understand their stored knowledge. 
+
+Generate a search query based on the user question which will be sent in the <question> tags. 
+You are to clean the user question so that it can be used as a query for a RAG-based AI assistant. 
+
+- Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
+- If the question is not in English, translate the question to English before generating the search query. 
+- If you cannot generate a search query, return just the number 0.
+- If the question does not make any sense or is not a question return just the number 0.`
+
 export const createUserPrompt = (contextList: NoteContext[] | undefined, question: string): string => {
   const formattedContexts = contextList?.map(context => {
     return `
@@ -37,5 +48,11 @@ export const createUserPrompt = (contextList: NoteContext[] | undefined, questio
     <question>${question}</question>
 
     ${formattedContexts}
+  `
+}
+
+export const createToCleanQuestionPrompt = (question: string): string => {
+  return `
+    <question>${question}</question>
   `
 }
